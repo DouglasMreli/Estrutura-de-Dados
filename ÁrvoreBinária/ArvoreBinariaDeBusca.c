bf0cbbc2c32fa8240d77b4d99651c9482c39bc3a
Douglas Mareli <82533319+DouglasMreli@users.noreply.github.com>
2021-08-01 15:51:31 -0300

    Update ArvoreBinariaDeBusca.c

--- ÁrvoreBinária/ArvoreBinariaDeBusca.c
+++ ÁrvoreBinária/ArvoreBinariaDeBusca.c
@@ -1,6 +1,3 @@
-// Grupo: Ayla Rodrigues, Douglas Mareli, Sofia Kitaeva
-// Exercicio 3 - Estrutura de dados
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
@@ -211,25 +208,25 @@ int main(){
 
 	while(opcao!=8)
 	{
-		printf("O que deseja fazer?\n 1.Ler ·rvore\n 2.Imprimir ·rvore\n 3.Verificar existÍncia de um elemento\n 4.Imprimir nÌvel de um nÛ\n 5.Imprimir as folhas menores que um valor\n 6.Inserir um nÛ na ·rvore\n 7.Remover um nÛ da ·rvore\n 8.Sair\n>");
+		printf("O que deseja fazer?\n 1.Ler √°rvore\n 2.Imprimir √°rvore\n 3.Verificar exist√™ncia de um elemento\n 4.Imprimir n√≠vel de um n√≥\n 5.Imprimir as folhas menores que um valor\n 6.Inserir um n√≥ na √°rvore\n 7.Remover um n√≥ da √°rvore\n 8.Sair\n>");
 		scanf("%d", &opcao);
 		printf("\n");
 		
 		if (opcao==1)//leitua da arvore
 		{
 			char arv[100]; 
-			printf("Qual ·rvore deseja ler?\n>");
+			printf("Qual √°rvore deseja ler?\n>");
 			scanf("%s", arv);
 			arq = fopen(arv, "rt");
 			printf("-----------\n");
-			printf("¡rvore Lida\n");
+			printf("√Årvore Lida\n");
 			printf("-----------\n");
 			a = LerArvore(arq, a);
 		}
 		else if(opcao==2)//impressao arvore
 		{
 			int escolha=0;
-			printf("Como deseja que seja feita a impress„o da ·rvore?\n 1.PrÈ-Ordem\n 2.Em-Ordem\n 3.PÛs-Ordem\n 4.Em largura\n");
+			printf("Como deseja que seja feita a impress√£o da √°rvore?\n 1.Pr√©-Ordem\n 2.Em-Ordem\n 3.P√≥s-Ordem\n 4.Em largura\n");
 			scanf("%d", &escolha);
 			if (escolha==1){
 				printf("----------\n");
@@ -260,25 +257,25 @@ int main(){
 		else if(opcao==3)//verificar existencia
 		{
 			int x;
-			printf("Qual o n˙mero que deseja verificar na ·rvore?\n");
+			printf("Qual o n√∫mero que deseja verificar na √°rvore?\n");
 			scanf("%d", &x);
 			x = Existe(a, x);
 
 			if (x==1){
 				printf("----------------------------\n");
-				printf("Esse n˙mero existe na ·rvore\n");
+				printf("Esse n√∫mero existe na √°rvore\n");
 				printf("----------------------------\n");
 			}
 			else{
 				printf("--------------------------------\n");
-				printf("Esse n˙mero n„o existe na ·rvore\n");
+				printf("Esse n√∫mero n√£o existe na √°rvore\n");
 				printf("--------------------------------\n");
 			}
 		}
-		else if (opcao==4)//imprimir nÌvel de nÛ
+		else if (opcao==4)//imprimir n√≠vel de n√≥
 		{
 			int no;
-			printf("De qual nÛ vocÍ deseja imprimir o nÌvel?\n");
+			printf("De qual n√≥ voc√™ deseja imprimir o n√≠vel?\n");
 			scanf("%d",&no);
 			printf("\n----------\n");
 			NivelNo(a,no,0);
@@ -293,21 +290,21 @@ int main(){
 			ImprimirMenores(a, x);
 			printf("----------\n");
 		}
-		else if(opcao==6)//inserir nÛ
+		else if(opcao==6)//inserir n√≥
 		{
 			int x;
-			printf("Qual elemento vocÍ deseja inserir?\n");
+			printf("Qual elemento voc√™ deseja inserir?\n");
 			scanf("%d",&x);
 			a = Inserir(a, x);
 			printf("-----------------\nElemento Inserido\n-----------------\n");
 		}
-		else if(opcao==7)//remover nÛ
+		else if(opcao==7)//remover n√≥
 		{
 			int rem;
-			printf("Qual nÛ deseja remover?\n>");
+			printf("Qual n√≥ deseja remover?\n>");
 			scanf("%d", &rem);
 			printf("-----------\n");
-			printf("NÛ Removido\n");
+			printf("N√≥ Removido\n");
 			Remover(a, rem);
 			printf("-----------\n");
 		}
@@ -316,7 +313,7 @@ int main(){
 		system("cls");
 	}
 		a = Destruir(a);
-		printf("O usu·rio saiu!\n");
+		printf("O usu√°rio saiu!\n");
 		fclose(arq);
 	return 0;
 }
